be8fa7e5e03c7c701d854cbbfe1071a413c6e3e7
kellymedia <mk@uconn.edu>
2020-09-26 21:24:59 -0400

    update manager test

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,5 +1,4 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../lib/manager");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
