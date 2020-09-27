c81db87fa1e12cb39009bcdbbbdcd8e5615bb8f3
kellymedia <mk@uconn.edu>
2020-09-26 21:01:03 -0400

    update tests and main

--- templates/main.js
+++ templates/main.js
@@ -1,4 +1,4 @@
-function mainHTML(data) {
+function main(data) {
     return `
       <!DOCTYPE html>
       <html lang="en">
@@ -7,7 +7,7 @@ function mainHTML(data) {
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <title>My Team</title>
+          <title>Employeer Roster</title>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
           <script src="https://kit.fontawesome.com/73f6284828.js" crossorigin="anonymous"></script>
 
@@ -48,5 +48,5 @@ function mainHTML(data) {
   `;
   }
   // export mainRender
-  module.exports = mainHTML;
+  module.exports = main;
   
